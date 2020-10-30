c4d9166664623a3ef29b6c5e32f7bb097a1ca0c7
DajakStipe <dajakstipe@gmail.com>
2020-10-31 00:06:19 +0100

    Implemented Place order screen

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,169 @@
+import React, { useState } from "react";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import CheckoutSteps from "../components/CheckoutSteps";
+import Message from "../components/Message.js";
+
+const PlaceOrderScreen = () => {
+	const cart = useSelector((state) => state.cart);
+	console.log(cart.paymentMethod);
+	const { paymentMethod } = cart;
+
+	//calculate prices
+	const addDecimals = (numb) => {
+		return (Math.round(numb * 100) / 100).toFixed(2);
+	};
+	cart.itemsPrice = addDecimals(
+		cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+	);
+
+	const freeDelivery = Number(100 - Number(cart.itemsPrice)).toFixed(0);
+	console.log(freeDelivery);
+	cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 20);
+	cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+	cart.totalPrice =
+		Number(cart.itemsPrice) +
+		Number(cart.taxPrice) +
+		Number(cart.shippingPrice);
+
+	const placeOrderHandler = () => {
+		console.log("object");
+	};
+
+	return (
+		<>
+			<CheckoutSteps step1 step2 step3 step4 />
+
+			<Row>
+				<Col md={8}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h2>Shipping Info</h2>
+							<p>
+								<strong className='font-weight-bold text-uppercase'>
+									Address:{" "}
+								</strong>
+								{cart.shippingAddress.address},{" "}
+								{cart.shippingAddress.city},
+								{cart.shippingAddress.postalCode},{" "}
+								{cart.shippingAddress.country}
+							</p>
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<h2>Payment Method</h2>
+							<strong className='font-weight-bold text-uppercase'>
+								Method:
+							</strong>
+							{paymentMethod}
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<h2>Order Items</h2>
+							{cart.cartItems.length === 0 ? (
+								<Message>Your Cart is Empty</Message>
+							) : (
+								<ListGroup variant='flush'>
+									{cart.cartItems.map((item, index) => (
+										<ListGroup.Item key={index}>
+											<Row>
+												<Col md={2}>
+													<Image
+														src={item.image}
+														alt={item.name}
+														fluid
+														rounded
+													/>
+												</Col>
+												<Col>
+													<Link
+														to={`/product/${item.product}`}
+													>
+														{item.name}
+													</Link>
+												</Col>
+												<Col md={4}>
+													<span className='float-right'>
+														{item.qty} x $
+														{item.price} = $
+														{item.qty * item.price}
+													</span>
+												</Col>
+											</Row>
+										</ListGroup.Item>
+									))}
+								</ListGroup>
+							)}
+						</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={4}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<h2>Order Summary</h2>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Items</Col>
+									<Col>
+										${cart.itemsPrice}
+										<br></br>
+										{freeDelivery > 0 ? (
+											<span className='font-weight-light font-italic'>
+												${freeDelivery} away from free
+												shipping
+											</span>
+										) : (
+											""
+										)}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Shipping</Col>
+									<Col>${cart.shippingPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Tax</Col>
+									<Col>${cart.taxPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>
+										<span className='boldthisthing'>
+											Total
+										</span>{" "}
+									</Col>
+									<Col>
+										{" "}
+										<span className='boldthisthing'>
+											${cart.totalPrice}
+										</span>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Button
+									type='button'
+									className='btn-block'
+									disabled={cart.cartItems.length === 0}
+									onClick={placeOrderHandler}
+								>
+									Complete Order
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
+};
+
+export default PlaceOrderScreen;
