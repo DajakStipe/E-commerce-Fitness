e42898b4468d1758e0a61b4930477e2418a731ea
DajakStipe <dajakstipe@gmail.com>
2020-10-31 18:39:27 +0100

    Added orderId screen

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -2,6 +2,9 @@ import {
 	ORDER_CREATE_FAIL,
 	ORDER_CREATE_SUCCESS,
 	ORDER_CREATE_REQUEST,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+	ORDER_DETAILS_FAIL,
 } from "../constants/orderConstants";
 import axios from "axios";
 
@@ -37,5 +40,35 @@ export const createOrder = (order) => async (dispatch, getState) => {
 					? error.response.data.message
 					: error.message,
 		});
+	}
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDER_DETAILS_REQUEST,
+		});
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		const { data } = await axios.get(`/api/orders/${id}`, config);
+
+		dispatch({
+			type: ORDER_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		const message =
+			error.response && error.response.data.message
+				? error.response.data.message
+				: error.message;
 	}
 };
---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,180 @@
+import React, { useEffect } from "react";
+import { Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import Message from "../components/Message.js";
+import Loader from "../components/Loader.js";
+import { getOrderDetails } from "../actions/orderActions";
+
+const OrderScreen = ({ match }) => {
+	const orderId = match.params.id;
+	const dispatch = useDispatch();
+
+	const orderDetails = useSelector((state) => state.orderDetails);
+	const { order, loading, error } = orderDetails;
+
+	const userLogin = useSelector((state) => state.userLogin);
+	const { userInfo } = userLogin;
+
+	if (!loading) {
+		const addDecimals = (numb) => {
+			return (Math.round(numb * 100) / 100).toFixed(2);
+		};
+		order.itemsPrice = addDecimals(
+			order.orderItems.reduce(
+				(acc, item) => acc + item.price * item.qty,
+				0
+			)
+		);
+	}
+
+	useEffect(() => {
+		dispatch(getOrderDetails(orderId));
+	}, [dispatch]);
+
+	console.log(orderDetails);
+
+	return loading ? (
+		<Loader />
+	) : error ? (
+		<Message variant='danger'>{error}</Message>
+	) : (
+		<>
+			{" "}
+			<h1>Order: {order._id} </h1>
+			<Row>
+				<Col md={8}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h2>Shipping Info</h2>
+							<p>
+								<strong>Name: </strong> {order.user.name}
+							</p>
+							<p>
+								<a href={`mailto:${order.user.email}`}>
+									Email: {order.user.email}
+								</a>
+							</p>
+							<p>
+								<strong>Address: </strong>
+								{order.shippingAddress.address},{" "}
+								{order.shippingAddress.city},
+								{order.shippingAddress.postalCode},{" "}
+								{order.shippingAddress.country}
+							</p>
+							{order.isDelivered ? (
+								<Message variant='success'>
+									Delivered on {order.DeliveredAt}
+								</Message>
+							) : (
+								<Message variant='danger'>
+									Not Delivered
+								</Message>
+							)}
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<h2>Payment Method</h2>
+							<p>
+								<strong className='font-weight-bold text-uppercase'>
+									Method:
+								</strong>
+								{order.paymentMethod}
+							</p>
+							{order.isPaid ? (
+								<Message variant='success'>
+									Paid on {order.paidAt}
+								</Message>
+							) : (
+								<Message variant='danger'>Not Paid</Message>
+							)}
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<h2>Order Items</h2>
+							{order.orderItems.length === 0 ? (
+								<Message>Order is empty</Message>
+							) : (
+								<ListGroup variant='flush'>
+									{order.orderItems.map((item, index) => (
+										<ListGroup.Item key={index}>
+											<Row>
+												<Col md={2}>
+													<Image
+														src={item.image}
+														alt={item.name}
+														fluid
+														rounded
+													/>
+												</Col>
+												<Col>
+													<Link
+														to={`/product/${item.product}`}
+													>
+														{item.name}
+													</Link>
+												</Col>
+												<Col md={4}>
+													<span className='float-right'>
+														{item.qty} x $
+														{item.price} = $
+														{item.qty * item.price}
+													</span>
+												</Col>
+											</Row>
+										</ListGroup.Item>
+									))}
+								</ListGroup>
+							)}
+						</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={4}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<h2>Order Summary</h2>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Items</Col>
+									<Col>${order.itemsPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Shipping</Col>
+									<Col>${order.shippingPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>Tax</Col>
+									<Col>${order.taxPrice}</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								<Row>
+									<Col>
+										<span className='boldthisthing'>
+											Total
+										</span>{" "}
+									</Col>
+									<Col>
+										{" "}
+										<span className='boldthisthing'>
+											${order.totalPrice}
+										</span>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
+};
+
+export default OrderScreen;
