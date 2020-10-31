228ca0f11390db1c0fe306d43fbd03785d301d12
DajakStipe <dajakstipe@gmail.com>
2020-10-31 22:44:04 +0100

    Added isPaid to the backend and order pay reducer&action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -5,6 +5,8 @@ import {
 	ORDER_DETAILS_REQUEST,
 	ORDER_DETAILS_SUCCESS,
 	ORDER_DETAILS_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS,
 } from "../constants/orderConstants";
 import axios from "axios";
 
@@ -63,6 +65,44 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
 
 		dispatch({
 			type: ORDER_DETAILS_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		const message =
+			error.response && error.response.data.message
+				? error.response.data.message
+				: error.message;
+	}
+};
+
+export const payOrder = (orderId, paymentResult) => async (
+	dispatch,
+	getState
+) => {
+	try {
+		dispatch({
+			type: ORDER_PAY_REQUEST,
+		});
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		const { data } = await axios.put(
+			`/api/orders/${orderId}/pay`,
+			paymentResult,
+			config
+		);
+
+		dispatch({
+			type: ORDER_PAY_SUCCESS,
 			payload: data,
 		});
 	} catch (error) {
