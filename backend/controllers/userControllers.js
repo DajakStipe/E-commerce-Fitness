69b89462e9986fa5f2b46ac8f974403c32b972ae
DajakStipe <dajakstipe@gmail.com>
2020-11-01 21:47:57 +0100

    Added isAdmin middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -100,4 +100,13 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 		throw new Error("User Not Found");
 	}
 });
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+
+// @desc    Get all users
+// @route   GET /api/users/profile
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+	const users = await User.find({});
+	res.json(users);
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
